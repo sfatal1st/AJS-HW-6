a16a28e9840f2dc4894920453b96f74472158173
sfatal1st <105299426+sfatal1st@users.noreply.github.com>
2023-03-26 22:15:44 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import AddCard from "./addCard";
 import DeleteCard from "./deleteCard";
-import DragDrop from "./DragDrop";
+import DragDrop from "./dragDrop";
 
 const addCard = new AddCard();
 const deleteCard = new DeleteCard();
